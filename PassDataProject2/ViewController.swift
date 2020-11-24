2e4d42f916757b4bb5bdf857d223452153096c86
David Mikhailov <david.mikhailovv@gmail.com>
2020-11-24 16:34:45 +0400

    Initial commit

---
+++ PassDataProject2/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PassDataProject2
+//
+//  Created by Давид Михайлов on 05.11.2020.
+//  Copyright © 2020 Давид Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil )
+    }
+    
+    @IBAction func unwindSegueToMainScreen (segue: UIStoryboardSegue) {
+        guard segue.identifier == "unwindSegue" else {return}
+        guard let svc = segue.source as? SecondViewController else {return}
+        self.resultLabel.text = svc.label.text
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else {return}
+        dvc.login = loginTF.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
